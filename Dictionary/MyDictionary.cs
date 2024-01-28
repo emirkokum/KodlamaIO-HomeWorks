6f8aafdb3ceece65ca589a2625f0f9cba79f4456
Emir Kokum <emir_kokum_@hotmail.com>
2024-01-28 16:39:43 +0300

    first

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;    
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key,TValue value)
+        {
+
+            TKey[] keyTemp = keys;
+            TValue[] valueTemp = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < keyTemp.Length; i++)    
+            {
+                keys[i] = keyTemp[i];
+                values[i] = valueTemp[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+
+        public void ShowAll()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine($"{keys[i]} = {values[i]}");
+            }
+        }
+
+
+
+       
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using Dictionary;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+
+        myDictionary.Add(1, "John");
+        myDictionary.Add(2, "James");
+        myDictionary.Add(3, "Alex");
+        myDictionary.ShowAll();
+    }
+}
